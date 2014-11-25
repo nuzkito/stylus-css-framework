7888cbd3f2475f0bf062214b40430c662bd9a03d
abalozz <alivanprox@gmail.com>
2014-11-25 01:27:45 +0100

    Agregada una guía de estilos básica

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,21 @@ gulp.task('stylus', function () {
     .pipe(gulp.dest('./assets'));
 });
 
+gulp.task('stylus-docs', function () {
+  return gulp.src('./ui-docs/docs.styl')
+    .pipe(stylus({use: [nib()]}))
+    .pipe(gulp.dest('./ui-docs'));
+});
+
 gulp.task('stylus-watch', ['stylus'], function () {
   gulp.watch('./stylus/**/*.styl', ['stylus']);
 });
 
-gulp.task('watch', ['stylus-watch']);
+gulp.task('stylus-watch-docs', ['stylus-docs'], function () {
+  gulp.watch('./ui-docs/*.styl', ['stylus-docs']);
+});
+
+gulp.task('watch', ['stylus-watch', 'stylus-watch-docs']);
 
 
 
